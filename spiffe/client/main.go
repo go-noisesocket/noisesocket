566073b5e98d4f1f7102d0296cd47879b51adbba
Denis Kolegov <dnkolegov@gmail.com>
2019-01-10 16:46:39 +0700

    Add SPIFFE

---
+++ spiffe/client/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/flynn/noise"
+	"gopkg.in/noisesocket.v0"
+	h "gopkg.in/noisesocket.v0/spiffe/helpers"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+)
+
+var (
+	serverCA, serverRoot *x509.CertPool
+	clientCertPEM        []byte
+)
+
+func init() {
+	var err error
+	serverCA, err = h.GetCertPool("./keys/test1-IntermediateCA.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	serverRoot, err = h.GetCertPool("./keys/test1-RootCA.pem")
+	if err != nil {
+		panic(err)
+	}
+	clientCertPEM, err = ioutil.ReadFile("./keys/client-cert.pem")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	cli, err := StartClient()
+
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("GET", "https://127.0.0.1:12888/", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := cli.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("HTTP response code: %d \n", resp.StatusCode)
+
+	_, err = io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func StartClient() (*http.Client, error) {
+	pub1, _ := base64.StdEncoding.DecodeString("L9Xm5qy17ZZ6rBMd1Dsn5iZOyS7vUVhYK+zby1nJPEE=")
+	priv1, _ := base64.StdEncoding.DecodeString("TPmwb3vTEgrA3oq6PoGEzH5hT91IDXGC9qEMc8ksRiw=")
+
+	clientKeys := noise.DHKey{
+		Public:  pub1,
+		Private: priv1,
+	}
+
+	signature, err := h.Sign(pub1, "./keys/client-key.pem")
+
+	if err != nil {
+		return nil, err
+	}
+	info := &h.AuthInfo{Certificate: clientCertPEM, Signature: signature}
+
+	payload, err := json.Marshal(info)
+	if err != nil {
+		return nil, err
+	}
+
+	transport := &http.Transport{
+
+		DialTLS: func(network, addr string) (net.Conn, error) {
+			conn, err := noisesocket.Dial(addr, &noisesocket.ConnectionConfig{
+				StaticKey:      clientKeys,
+				Payload:        payload,
+				VerifyCallback: VerifyCallback,
+			})
+			if err != nil {
+				fmt.Println("Dial", err)
+			}
+			return conn, err
+		},
+	}
+	cli := &http.Client{
+		Transport: transport,
+	}
+
+	return cli, nil
+}
+
+func VerifyCallback(publicKey []byte, data []byte) error {
+	if len(publicKey) == 0 {
+		return nil
+	}
+
+	verifyOpts := x509.VerifyOptions{
+		Roots:         serverRoot,
+		Intermediates: serverCA,
+		KeyUsages: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageAny,
+		},
+	}
+
+	cert, signature, err := h.GetCertificateAndSignature(data)
+	if err != nil {
+		return fmt.Errorf("unable to get server certificate: %s", err)
+	}
+
+	_, err = cert.Verify(verifyOpts)
+	if err != nil {
+		return fmt.Errorf("unable to verify server certificate: %s", err)
+	}
+
+	err = h.ValidateSignature(publicKey, cert, signature)
+	if err != nil {
+		return fmt.Errorf("unable to verify noise server public key: %s", err)
+	}
+	return nil
+
+}
---
+++ spiffe/server/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/flynn/noise"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/noisesocket.v0"
+	h "gopkg.in/noisesocket.v0/spiffe/helpers"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	clientCA, clientRoot *x509.CertPool
+	serverCertPEM        []byte
+)
+
+var (
+	allowedURIs = kingpin.Flag("allow-uri", "Allow clients with given SPIFFE URI ID.").PlaceHolder("URI").Strings()
+)
+
+func init() {
+	var err error
+
+	clientCA, err = h.GetCertPool("./keys/test3-IntermediateCA.pem")
+	if err != nil {
+		panic(err)
+	}
+	clientRoot, err = h.GetCertPool("./keys/test3-RootCA.pem")
+	if err != nil {
+		panic(err)
+	}
+	serverCertPEM, err = ioutil.ReadFile("./keys/server-cert.pem")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	kingpin.Parse()
+
+	fmt.Printf("Allowed SPIFFE URI IDs: %d\n", len(*allowedURIs))
+	for _, u := range *allowedURIs {
+		fmt.Println(u)
+	}
+
+	err := startServer()
+	if err != nil {
+		panic("failed to start server: " + err.Error())
+	}
+}
+
+func startServer() error {
+	fmt.Println("Starting server...")
+
+	pub, _ := base64.StdEncoding.DecodeString("J6TRfRXR5skWt6w5cFyaBxX8LPeIVxboZTLXTMhk4HM=")
+	priv, _ := base64.StdEncoding.DecodeString("vFilCT/FcyeShgbpTUrpru9n5yzZey8yfhsAx6DeL80=")
+
+	serverKeys := noise.DHKey{
+		Public:  pub,
+		Private: priv,
+	}
+
+	signature, err := h.Sign(pub, "./keys/server-key.pem")
+	if err != nil {
+		return err
+	}
+
+	info := &h.AuthInfo{Certificate: serverCertPEM, Signature: signature}
+	payload, err := json.Marshal(info)
+	if err != nil {
+		return err
+	}
+
+	l, err := noisesocket.Listen(":12888", &noisesocket.ConnectionConfig{
+		StaticKey:      serverKeys,
+		VerifyCallback: verifyCallback,
+		Payload:        payload,
+	})
+
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+
+	server := &http.Server{
+		Addr:         ":12888",
+		ReadTimeout:  1 * time.Minute,
+		WriteTimeout: 1 * time.Minute,
+	}
+
+	server.Handler = http.HandlerFunc(Handler)
+	panic(server.Serve(l))
+}
+
+func verifyCallback(publicKey []byte, data []byte) error {
+	if len(publicKey) == 0 {
+		return nil
+	}
+
+	verifyOpts := x509.VerifyOptions{
+		Roots:         clientRoot,
+		Intermediates: clientCA,
+		KeyUsages: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageClientAuth,
+		},
+	}
+
+	cert, signature, err := h.GetCertificateAndSignature(data)
+	if err != nil {
+		return fmt.Errorf("unable to get client certificate: %s", err)
+	}
+
+	_, err = cert.Verify(verifyOpts)
+	if err != nil {
+		return fmt.Errorf("unable to veirfy client certificate: %s", err)
+	}
+
+	err = h.ValidateURI(cert, *allowedURIs)
+	if err != nil {
+		return fmt.Errorf("client URI is not allowed: %s", err)
+	}
+
+	err = h.ValidateSignature(publicKey, cert, signature)
+	if err != nil {
+		return fmt.Errorf("unable to verify noise client public key: %s", err)
+	}
+
+	if len(cert.URIs) != 1 {
+		return errors.New("failed to get SPIFFE ID")
+	}
+	err = h.VerifyURI(cert.URIs, *allowedURIs)
+	if err != nil {
+		return fmt.Errorf("access is not perrmitted: %s", err)
+	}
+	log.Printf("Access for %s entity has been permitted", cert.URIs[0])
+	return nil
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
